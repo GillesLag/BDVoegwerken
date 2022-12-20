b2f39e4f78650124dd9835f47cb0406459a387d1
r06658604 <r0658604@student.thomasmore.be>
2022-12-20 21:24:45 +0100

    inladen detailpagina

--- javascript/detailLaden.js
+++ javascript/detailLaden.js
@@ -1,26 +1,53 @@
-let voegwerken = document.querySelector("#imgVoegwerken");
-let gevelrenovatie = document.querySelector("#imgGevelrenovatie");
-let gevelreiniging = document.querySelector("#imgGevelreiniging");
-let gevelbescherming = document.querySelector("#imgGevelbescherming");
 let afbeeldingDetail = document.querySelector("#imgDetail");
-let h1Detail = document.querySelector("#h1Detail");
+let detailH1 = document.querySelector("#detailH1");
+let pDetail = document.querySelector("#detailUitleg");
+let keuze;
 
-//clickevents toevoegen
+//keuze uit localstorage halen
+keuze = localStorage.getItem('keuze');
 
-
+console.log(keuze);
 
 //functies voor het inladen
 let laadVoegwerken = () => {
-    console.log("clicked");
-    afbeeldingDetail.setAttribute.src = "/images/voegwerken.jpg";
-    afbeeldingDetail.setAttribute.alt = "Foto van de voegwerken";
-    h1Detail.textContent = "Voegwerken";
-
-    laadPagina;
+    afbeeldingDetail.src = "/images/voegwerken.jpg";
+    afbeeldingDetail.alt = "Foto van de voegwerken";
+    detailH1.textContent = "Voegwerken";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+let laadGevelrenovatie = () => {
+    afbeeldingDetail.src = "/images/gevelrenovatie.jpg";
+    afbeeldingDetail.alt = "Foto van de gevelrenovatie";
+    detailH1.textContent = "Gevelrenovatie";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+let laadGevelreiniging = () => {
+    afbeeldingDetail.src = "/images/gevelreiniging.jpg";
+    afbeeldingDetail.alt = "Foto van de voegwerken";
+    detailH1.textContent = "Gevelreiniging";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+let laadGevelbescherming = () => {
+    afbeeldingDetail.src = "/images/gevelbescherming.jpg";
+    afbeeldingDetail.alt = "Foto van de voegwerken";
+    detailH1.textContent = "Gevelbescherming";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 }
 
-let laadPagina = () => {
-    window.location = "detailpagina.html";
+//content kiezen
+switch (keuze) {
+    case "voegwerken":
+        laadVoegwerken();
+        break;
+    case "gevelrenovatie":
+        laadGevelrenovatie();
+        break;
+    case "gevelreiniging":
+        laadGevelreiniging();
+        break;
+    case "gevelbescherming":
+        laadGevelbescherming();
+        break;
+    default:
+        break;
 }
-
-voegwerken.addEventListener("click", laadVoegwerken);
