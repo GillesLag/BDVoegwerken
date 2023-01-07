913ab04cbe0591af5f5ef197e9a7b7faa2d6c898
r06658604 <r0658604@student.thomasmore.be>
2023-01-07 18:59:26 +0100

    final

--- javascript/addLocalStorage.js
+++ javascript/addLocalStorage.js
@@ -1,84 +1,112 @@
-let btnProject1 = document.querySelector("#btnProject1");
-let imgProject1 = document.querySelector("#imgProject1");
-let btnProject2 = document.querySelector("#btnProject2");
-let imgProject2 = document.querySelector("#imgProject2");
-let btnProject2Small = document.querySelector("#btnProject2Small");
-let imgProject2Small = document.querySelector("#imgProject2Small");
-let btnProject3 = document.querySelector("#btnProject3");
-let imgProject3 = document.querySelector("#imgProject3");
-let voegwerken = document.querySelector("#imgVoegwerken");
-let gevelrenovatie = document.querySelector("#imgGevelrenovatie");
-let gevelreiniging = document.querySelector("#imgGevelreiniging");
-let gevelbescherming = document.querySelector("#imgGevelbescherming");
+//html-elementen definiëren
+const btnProject1 = document.querySelector("#btnProject1");
+const imgProject1 = document.querySelector("#imgProject1");
+const btnProject2 = document.querySelector("#btnProject2");
+const imgProject2 = document.querySelector("#imgProject2");
+const btnProject2Small = document.querySelector("#btnProject2Small");
+const imgProject2Small = document.querySelector("#imgProject2Small");
+const btnProject3 = document.querySelector("#btnProject3");
+const imgProject3 = document.querySelector("#imgProject3");
+const voegwerken = document.querySelector("#imgVoegwerken");
+const gevelrenovatie = document.querySelector("#imgGevelrenovatie");
+const gevelreiniging = document.querySelector("#imgGevelreiniging");
+const gevelbescherming = document.querySelector("#imgGevelbescherming");
+const liProject1 = document.querySelector("#liProject1");
+const liProject2 = document.querySelector("#liProject2");
+const liProject3 = document.querySelector("#liProject3");
 
-let keuze;
+let choice;
 
+//de keuze toevoegen aan de localstorage en de detailpagina openen
 let laadDetail = () => {
-    if (keuze == "voegwerken") {
+    if (choice == "voegwerken") {
         localStorage.clear();
         localStorage.setItem('keuze', 'voegwerken');
-    } else if (keuze == "gevelrenovatie") {
+    } else if (choice == "gevelrenovatie") {
         localStorage.clear();
         localStorage.setItem('keuze', 'gevelrenovatie');
-    } else if (keuze == "gevelreiniging") {
+    } else if (choice == "gevelreiniging") {
         localStorage.clear();
         localStorage.setItem('keuze', 'gevelreiniging');
-    } else {
+    } else if (choice == "gevelbescherming") {
         localStorage.clear();
         localStorage.setItem('keuze', 'gevelbescherming');
+    } else if (choice == "project1") {
+        localStorage.clear();
+        localStorage.setItem('keuze', 'project1');
+    } else if (choice == "project2") {
+        localStorage.clear();
+        localStorage.setItem('keuze', 'project2');
+    } else {
+        localStorage.clear();
+        localStorage.setItem('keuze', 'project3');
     }
 
     window.location = "detailpagina.html";
 }
 
+//eventlisteners aanmaken en de functie laadDetail oproepen
+liProject1.addEventListener("click", function() {
+    choice = "project1";
+    laadDetail();
+});
+liProject2.addEventListener("click", function() {
+    choice = "project2";
+    laadDetail();
+});
+liProject3.addEventListener("click", function() {
+    choice = "project3";
+    laadDetail();
+});
+
 if (btnProject1 != null && imgProject1 != null && btnProject2 != null && imgProject2 != null && btnProject2Small != null && imgProject2Small != null && btnProject3 != null && imgProject3 != null) {
     btnProject1.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project1";
         laadDetail();
     });
     imgProject1.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project1";
         laadDetail();
     });
     btnProject2.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project2";
         laadDetail();
     });
     imgProject2.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project2";
         laadDetail();
     });
     btnProject2Small.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project2";
         laadDetail();
     });
     imgProject2Small.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project2";
         laadDetail();
     });
     btnProject3.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project3";
         laadDetail();
     });
     imgProject3.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "project3";
         laadDetail();
     });
 } else {
     voegwerken.addEventListener("click", function() {
-        keuze = "voegwerken";
+        choice = "voegwerken";
         laadDetail();
     });
     gevelrenovatie.addEventListener("click", function() {
-        keuze = "gevelrenovatie";
+        choice = "gevelrenovatie";
         laadDetail();
     });
     gevelreiniging.addEventListener("click", function() {
-        keuze = "gevelreiniging";
+        choice = "gevelreiniging";
         laadDetail();
     });
     gevelbescherming.addEventListener("click", function() {
-        keuze = "gevelbescherming";
+        choice = "gevelbescherming";
         laadDetail();
     });
 }
--- javascript/detailLaden.js
+++ javascript/detailLaden.js
@@ -1,14 +1,11 @@
 let afbeeldingDetail = document.querySelector("#imgDetail");
 let detailH1 = document.querySelector("#detailH1");
 let pDetail = document.querySelector("#detailUitleg");
-let keuze;
 
 //keuze uit localstorage halen
-keuze = localStorage.getItem('keuze');
+choice = localStorage.getItem('keuze');
 
-console.log(keuze);
-
-//functies voor het inladen
+//functies voor het opvullen van de pagina met inhoud
 let laadVoegwerken = () => {
     afbeeldingDetail.alt = "Foto van de voegwerken";
     detailH1.textContent = "Voegwerken";
@@ -32,8 +29,24 @@ let laadGevelbescherming = () => {
     detailH1.textContent = "Gevelbescherming";
     pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 }
+let laadProject1 = () => {
+    afbeeldingDetail.alt = "project 1";
+    detailH1.textContent = "Project 1";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+let laadProject2 = () => {
+    afbeeldingDetail.alt = "project 2";
+    detailH1.textContent = "Project 2";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
+let laadProject3 = () => {
+    afbeeldingDetail.src = "/images/DSCN1580.JPG";
+    afbeeldingDetail.alt = "project 3";
+    detailH1.textContent = "Project 3";
+    pDetail.textContent = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
+}
 async function afbeeldingenVoegwerken() {
-    const afbeeldingen = ["/images/DSCN1572.JPG", "/images/DSCN1574.JPG", "/images/DSCN1575.JPG", "/images/DSCN1577.JPG", "/images/DSCN1578.JPG", "/images/DSCN1579.JPG", "/images/DSCN1580.JPG", "/images/DSCN1581.JPG", "/images/DSCN1585.JPG", "/images/DSCN1589.JPG", "/images/DSCN1590.JPG", "/images/DSCN1592.JPG"];
+    const afbeeldingen = ["/images/DSCN1572.JPG", "/images/DSCN1581.JPG", "/images/DSCN1585.JPG", "/images/DSCN1589.JPG", "/images/DSCN1590.JPG", "/images/DSCN1592.JPG", "/images/voegwerken.jpg"];
     for (let i = 0; i < afbeeldingen.length; i++) {
         afbeeldingDetail.src = afbeeldingen[i];
         await wachten(4000);
@@ -41,13 +54,31 @@ async function afbeeldingenVoegwerken() {
     i = 0;
     afbeeldingenVoegwerken();
 }
+async function afbeeldingenProject1() {
+    const afbeeldingen = ["/images/DSCN1574.JPG", "/images/DSCN1575.JPG", "/images/DSCN1577.JPG"];
+    for (let i = 0; i < afbeeldingen.length; i++) {
+        afbeeldingDetail.src = afbeeldingen[i];
+        await wachten(4000);
+    }
+    i = 0;
+    afbeeldingenProject1();
+}
+async function afbeeldingenProject2() {
+    const afbeeldingen = ["/images/DSCN1578.JPG", "/images/DSCN1579.JPG"];
+    for (let i = 0; i < afbeeldingen.length; i++) {
+        afbeeldingDetail.src = afbeeldingen[i];
+        await wachten(4000);
+    }
+    i = 0;
+    afbeeldingenProject2();
+}
 
 function wachten(ms) {
     return new Promise(val => setTimeout(val, ms));
 }
 
-//content kiezen
-switch (keuze) {
+//kiezen welke content ingeladen moet worden
+switch (choice) {
     case "voegwerken":
         laadVoegwerken();
         afbeeldingenVoegwerken();
@@ -60,6 +91,17 @@ switch (keuze) {
         break;
     case "gevelbescherming":
         laadGevelbescherming();
+        break;
+    case "project1":
+        afbeeldingenProject1();
+        laadProject1();
+        break;
+    case "project2":
+        afbeeldingenProject2();
+        laadProject2();
+        break;
+    case "project3":
+        laadProject3();
         break;
     default:
         break;
